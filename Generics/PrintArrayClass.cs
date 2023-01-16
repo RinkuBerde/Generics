b1acb6c4295a3479545e2600ff0213d6eca8662b
rinkuberde <rinkuberde@gmail.com>
2023-01-16 22:11:56 +0530

    [Rinku] - Generics Class

---
+++ Generics/PrintArrayClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public  class PrintArrayClass<T>
+    {
+       private T[] inputArray;
+       public PrintArrayClass(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint()
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,10 +10,12 @@
             char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
             bool[] boolArr = { true, false };
 
+            // for normal methods
             // PrintArray.toPrint(intArray);
             // PrintArray.toPrint(doubleArray);
             //PrintArray.toPrint(charArray);
 
+            //for generics methods
              PrintArray.toPrint<int>(intArray);
              PrintArray.toPrint<double>(doubleArray);
              PrintArray.toPrint<char>(charArray);
@@ -21,6 +23,11 @@
             //PrintArray<int> obj = new PrintArray<int>(intArray);
             //obj.toPrint();
 
+            //for generics class
+            new PrintArrayClass<int>(intArray).toPrint();
+            new PrintArrayClass<double>(doubleArray).toPrint();
+            new PrintArrayClass<char>(charArray).toPrint();
+            new PrintArrayClass<bool>(boolArr).toPrint();
         }
     }
 }
