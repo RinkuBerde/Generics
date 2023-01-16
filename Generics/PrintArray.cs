c32119c94123d7f38cb41a52b3a761a8b8e46ed0
rinkuberde <rinkuberde@gmail.com>
2023-01-16 20:08:12 +0530

    [Rinku] - Generics Method

---
+++ Generics/PrintArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintArray
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (T element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+       //  public static void toPrint(int[] inputArray)
+       //  {
+       //      foreach (var element in inputArray)
+       //       {
+       //           Console.WriteLine(element);
+       //      }
+       //       Console.WriteLine("----------------------------------");
+       //  }
+       // public static void toPrint(double[] inputArray)
+       //  {
+       //      foreach (var element in inputArray) 
+                 
+         //     {
+         //       Console.WriteLine(element);
+        //     }
+        //     Console.WriteLine("----------------------------------");
+       // }
+
+        //public static void toPrint(char[] inputArray)
+        //{
+        //    foreach (var element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("-------------------------------------");
+        //}
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics!!!");
+
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            bool[] boolArr = { true, false };
+
+            // PrintArray.toPrint(intArray);
+            // PrintArray.toPrint(doubleArray);
+            //PrintArray.toPrint(charArray);
+
+             PrintArray.toPrint<int>(intArray);
+             PrintArray.toPrint<double>(doubleArray);
+             PrintArray.toPrint<char>(charArray);
+             PrintArray.toPrint<bool>(boolArr);
+            //PrintArray<int> obj = new PrintArray<int>(intArray);
+            //obj.toPrint();
+
         }
     }
 }
